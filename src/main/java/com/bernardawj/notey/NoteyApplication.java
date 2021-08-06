6213c340700414e42c92ffec31c69c369ab48827
Bernard Ang <work@bernardawj.com>
2021-08-06 10:45:32 +0800

    Added system.properties to run java 11

--- src/main/java/com/bernardawj/notey/NoteyApplication.java
+++ src/main/java/com/bernardawj/notey/NoteyApplication.java
@@ -2,12 +2,14 @@ package com.bernardawj.notey;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
 public class NoteyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NoteyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NoteyApplication.class, args);
+    }
 
 }
